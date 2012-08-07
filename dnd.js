51d3cdb7cdf89da592a1e7d0f7728b9c150321fa
Pascal Precht <pascal.precht@googlemail.com>
2012-08-07 22:22:51 +0200

    Added support for adding dragevents to more then one elements, this also enables drag and drop within the browser through different elements

--- dnd.js
+++ dnd.js
@@ -1,31 +1,39 @@
-;window.dnd = Object.create({
+;window.dnd = {
 
     on: function( expression, config ) {
 
-        var el = document.querySelector(expression);
+      var els = document.querySelectorAll(expression);
 
-        el.addEventListener('dragenter', function(e) {
-            e.stopPropagation();
-            e.preventDefault();
-            config.enter ? config.enter(this) : 0;
+      [].forEach.call(els, function (el) {
+
+        el.addEventListener('dragstart', function (e) {
+          e.dataTransfer.effectAllowed = 'move';
+          config.start ? config.start(this, e) : 0;
         }, false);
 
-        el.addEventListener('dragover',  function(e) {
-            e.stopPropagation();
-            e.preventDefault();
-            config.over ? config.over(this) : 0;
+        el.addEventListener('dragenter', function (e) {
+          config.enter ? config.enter(this, e) : 0;
         }, false);
 
-        el.addEventListener('dragleave', function(e) {
-            e.stopPropagation();
-            e.preventDefault();
-            config.leave ? config.leave(this) : 0;
+        el.addEventListener('dragover', function (e) {
+          e.preventDefault();
+          e.dataTransfer.dropEffect = 'move';
+          config.over ? config.over(this, e) : 0;
+        }, false);
+
+        el.addEventListener('dragleave', function (e) {
+          config.leave ? config.leave(this, e) : 0;
         }, false );
 
-        el.addEventListener('drop', function(e) {
-            e.stopPropagation();
-            e.preventDefault();
-            config.drop ? config.drop(e.dataTransfer.files, this) : 0;
+        el.addEventListener('drop', function (e) {
+          e.stopPropagation();
+          e.preventDefault();
+          config.drop ? config.drop(e.dataTransfer, this, e) : 0;
         }, false);
+
+        el.addEventListener('dragend', function (e) {
+          config.end ? config.end(this, e) : 0;
+        }, false);
+     });
   }
-});
+};
