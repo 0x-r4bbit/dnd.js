54cfff7143740e451953a2e6531abc196e4282e4
Pascal Precht <pascal.precht@googlemail.com>
2012-08-28 09:33:09 +0200

    building drag and drop example" " "

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
-var span = document.querySelector('.droparea span');
-
 dnd.on('.droparea', {
   'enter': function (el) {
-    span.innerHTML = "Allright, drop'em!";
+    el.innerHTML = "Allright, drop'em!";
     el.classList.add('dropping');
   },
   'leave': function (el) {
-    span.innerHTML = "Drop some files here!";
+    el.innerHTML = "Drop some files here!";
     el.classList.remove('dropping');
   },
   'drop': function (data, el, e) {
-    span.innerHTML = "Dropped!";
-    console.dir(data.files);
+    el.innerHTML = "Dropped, take a look at your console!";
+    [].forEach.call(data.files, function (file, i) {
+      console.dir(file);
+    });
     el.classList.remove('dropping');
   }
+});
+
+dnd.on('.column', {
 });
