5badcde66e1c713b39794884207457d0afa3a12a
Pascal Precht <pascal.precht@googlemail.com>
2012-08-28 10:14:01 +0200

    Built dragout example

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+var dndEl = null,
+    file = document.querySelector('.download-file'),
+    fileDetails;
+
 dnd.on('.droparea', {
   'enter': function (el) {
     el.innerHTML = "Allright, drop'em!";
@@ -17,4 +21,38 @@ dnd.on('.droparea', {
 });
 
 dnd.on('.column', {
+  'start': function (el, e) {
+    el.style.opacity = '0.4';
+
+    dndEl = el;
+    e.dataTransfer.setData('text/html', el.innerHTML);
+  },
+  'enter': function (el) {
+    el.classList.add('dropping');
+  },
+  'leave': function (el) {
+    el.classList.remove('dropping');
+  },
+  'drop': function (data, el, e) {
+    if (dndEl != el) {
+      dndEl.innerHTML = el.innerHTML;
+      el.innerHTML = data.getData('text/html');
+    }
+    el.classList.remove('dropping');
+  },
+  'end': function (el) {
+    el.style.opacity = '1';
+  }
+});
+
+if (typeof(file.dataset) === 'undefined') {
+  fileDetails = file.getAttribute('data-downloadurl');
+} else {
+  fileDetails = file.dataset.downloadurl;
+}
+
+dnd.on('.download-file', {
+  'start': function (el, e) {
+    e.dataTransfer.setData('DownloadURL', fileDetails);
+  }
 });
